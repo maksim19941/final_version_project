396916d6c922ffb3b4f1079f9f282fa5c4f16b45
sergey <mysmurov@gmail.com>
2024-03-05 09:02:39 +0300

    antifraud start

--- antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
+++ antifraud/src/main/java/com/bank/antifraud/AntifraudApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AntifraudApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(AntifraudApplication.class, args);
+
     }
 }
